3286a1caa590c687da06ebaa61f1baaee5870715
Katy600 <katherine.mccann@digital.justice.gov.uk>
2017-10-31 10:53:39 +0000

    Remove second_name and gender from model and add spouse. Migrate changes.

--- table/models.py
+++ table/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class GuestList(models.Model):
-	first_name = models.CharField(max_length=200)
-	second_name = models.CharField(max_length=200)
-	gender = models.CharField(max_length = 200)
+	first_name = models.CharField(max_length=40)
+	spouse = models.CharField(max_length = 40)
 
 	def __str__(self):
 		return self.first_name
