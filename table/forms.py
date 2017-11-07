66f748fa4b5d50b07dc63d0a78d41b49cef00bab
Katy600 <katherine.mccann@digital.justice.gov.uk>
2017-11-07 13:46:51 +0000

    Is able to add new guest to the list with the new guest view. Error's if you add a guest that already exists

--- table/forms.py
+++ table/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from table.models import GuestList
+from django.forms import CharField
 
 class CreateNewGuests(forms.Form):
 	add_new_guest = forms.CharField(required=True)
 	add_spouse = forms.CharField(required=False, initial='None')
+
+# class SlugField(CharField):
+#     default_validators = [validators.validate_slug]
--- table/views.py
+++ table/views.py
@@ -12,44 +12,49 @@ from django import forms
 
 
 def view_guest_list(request):
-	guestlist = GuestList.objects.all()
-	# return render(request, 'table/table_plan.html')
-	return TemplateResponse(request, 'table_plan.html', {'guestlist': GuestList.objects.all()})
+  guestlist = GuestList.objects.all()
+  # return render(request, 'table/table_plan.html')
+  return TemplateResponse(request, 'table_plan.html', {'guestlist': GuestList.objects.all()})
 
 def search_guests(request):
-	# return render(request, 'table/search_guests.html')
-	
-	return TemplateResponse(request, 'search_guests.html', {'guestlist': GuestList.objects.all()})
+# return render(request, 'table/search_guests.html')
+  return TemplateResponse(request, 'search_guests.html', {'guestlist': GuestList.objects.all()})
 
 def search(request):
-	errors = []
-	if 'name' in request.GET:
-		name = request.GET['name']
-		if not name:
-			errors.append('Enter a guests name.')
-			return render( request, 'table/search_guests.html', {'errors' : errors})
-		elif len(name) > 20:
-			errors.append('Please enter at most 20 characters.')
-			return render( request, 'table/search_guests.html', {'errors' : errors})
-		else:
-			guests = GuestList.objects.filter(first_name__icontains=name)
-			return render(request, 'table/see_results.html',
-						{'guests': guests, 'name': name})
+  errors = []
+  if 'name' in request.GET:
+    name = request.GET['name']
+    if not name:
+      errors.append('Enter a guests name.')
+      return render( request, 'table/search_guests.html', {'errors' : errors})
+    elif len(name) > 20:
+      errors.append('Please enter at most 20 characters.')
+      return render( request, 'table/search_guests.html', {'errors' : errors})
+    else:
+      guests = GuestList.objects.filter(first_name__icontains=name)
+      return render(request, 'table/see_results.html',
+            {'guests': guests, 'name': name})
 
 def new_guests(request):
-	if request.method == 'POST':
-		form = CreateNewGuests(request.POST)
-		if form.is_valid():
-			cd = form.cleaned_data
-			cd['add_new_guest'],
-			cd['add_spouse']
-			
-				
-		return HttpResponseRedirect('/create-new-guest/completed/')
-	else:
-			form = CreateNewGuests()
+  error = False
+  if request.method == 'POST':
+    form = CreateNewGuests(request.POST)
+    if form.is_valid():
+      guest = form.cleaned_data['add_new_guest']
+      spouse = form.cleaned_data['add_spouse']
+      new_guest = request.POST['add_new_guest']
+      guest_in_database = GuestList.objects.filter(first_name__icontains=new_guest)
+      if guest_in_database:
+        error = True
+        return render(request, 'enter_guest_details.html', {'form': form, 'error': error})
+      elif error == False:
+        guest_entered = GuestList(first_name=guest, spouse=spouse)
+        guest_entered.save()
+        return HttpResponseRedirect('/create-new-guest/completed/')
+  else:
+      form = CreateNewGuests()
+   
+      return render(request, 'enter_guest_details.html', {'form': form, 'error': error})
 
-			return render(request, 'enter_guest_details.html', {'form': form})
 
-
-			
+      
